4333ab94669a4d397803350f45df58be32c73dc4
Игорь_Подолян <tinyxxl@bk.ru>
2025-12-01 13:45:06 +0400

    completed first task

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,38 @@
+const mealAjaxUrl = "user/meals/";
+
+// https://stackoverflow.com/a/5064235/548473
+const ctx = {
+    ajaxUrl: mealAjaxUrl
+};
+
+$(function () {
+    makeEditable(
+        $('#mealsTable').DataTable({
+            "paging": false,
+            "info": true,
+            "columns": [
+                {
+                    "data": "dateTime"
+                },
+                {
+                    "data": "description"
+                },
+                {
+                    "data": "calories"
+                },
+                {
+                    "defaultContent": "Update",
+                    "orderable": false
+                },
+                {
+                    "defaultContent": "Delete",
+                    "orderable": false
+                }
+            ],
+            order: [[0, 'desc']]
+        })
+    );
+});
+
+
+
